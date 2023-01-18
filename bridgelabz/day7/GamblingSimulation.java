a4de8dcd3d84b906852f123d26c4f35de265dc12
Bharti <bharatifule10gmail.com>
2023-01-18 13:34:48 +0530

    Added:GamblingSimulationProblem

--- bridgelabz/day7/GamblingSimulation.java
+++ bridgelabz/day7/GamblingSimulation.java
@@ -8,16 +8,25 @@ public class GamblingSimulation {
         int stake = 100;
         int bet = 1;
         Random random = new Random();
-       int num = random.nextInt(2) + 0;
+        System.out.println("Initial stake " + stake);
+        while (stake != 150 && stake != 50) {
+            int num = random.nextInt(2);
+            System.out.println("num is "+num);
+            if (num == 1) {
+                stake += bet;
 
-        if (num == 1){
-            stake += bet;
-            System.out.println("gambler is win by "+ bet+" & total Stake is " + stake);
+                //System.out.println("gambler is win by " + bet + " & total Stake is " + stake);
+            } else if (num == 0) {
+                stake -= bet;
+              //  System.out.println("gambler is loose by " + bet + " & total Stake is " + stake);
+            }
 
         }
-        else if (num == 0){
-            stake -=bet;
-            System.out.println("gambler is loose by "+bet+" & total Stake is "+ stake );
+        if (stake >= 150) {
+            System.out.println("player win 50% of its initial stake " + stake);
+        } else {
+            System.out.println("player loose 50% of its initial stake " + stake);
         }
+
     }
 }
