a52ddfe999e9df5c7c0fbc06c58e7b56c74226ba
Bharti <bharatifule10gmail.com>
2023-01-18 13:07:39 +0530

    Added:GamblingSimulationProblem

---
+++ README.md
@@ -0,0 +1,1 @@
+# Day7_Gambling-_Simulation-_Problem
---
+++ bridgelabz/day7/GamblingSimulation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day7;
+
+public class GamblingSimulation {
+public static void main(String[] args) {
+	System.out.println("Welcome to Gambling Simulation Problem");
+}
+}
